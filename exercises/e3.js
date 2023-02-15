f01e00d0db0bbf5ea183e19716b50106ea06915c
TheWerbinator <34259061+TheWerbinator@users.noreply.github.com>
2023-02-15 11:29:46 -0700

    Add files
    Add files from Master to Main, since I can't figure out how to pull from master directly in github because 'they are completely different' or something

---
+++ exercises/e3.js
@@ -0,0 +1,74 @@
+// FUNCTIONS: Early return
+// BEFORE YOU START: please read the /exercises-info/e3.md file.
+
+/**
+ * ====================================================
+ * Create a function that works as follows:
+ * The function name is 'doesArrayIncludeItemsBetweenVals'
+ * given an array of numbers. You can use it as the arr for testing purposes
+ * The function take an arr (array), val1 (number) and val2 (number) as arguments.
+ * The function returns a boolean if array includes an item that is greater than val1 and less than val2
+ * The function MUST make an early return if the item is found and use the default return otherwise
+ * doesArrayIncludeItemsBetweenVals([2, 4, 2], 3, 5) => true
+ * doesArrayIncludeItemsBetweenVals([2, 4, 2], 5, 10) => false
+*/
+
+const doesArrayIncludeItemsBetweenVals = (arr, val1, val2) => {
+  let solution = false;
+  arr.forEach(value => {
+    if(value > val1 && value < val2) {
+      solution = true
+    }
+  });
+  return solution
+}
+
+/**
+ * ====================================================
+ * Create three functions using 3 types of syntax: named, arrow and function expression (with anonymous function)
+ * Named function: getValueWithConditionOne;
+ * Arrow Function name: getValueWithConditionTwo;
+ * Variable name for the function expression: getValueWithConditionThree;
+ * All three functions resolve the same task:
+    * The function takes two numbers as arguments
+    * If the numbers are both equal to 40, return the sum of them
+    * Else return the doubled sum of them
+ * getValueWithConditionOne(40, 40) => 80;
+ * getValueWithConditionOne(20, 30) => 100;
+*/
+
+function getValueWithConditionOne(val1, val2) {
+  if(val1 === 40 && val2 === 40) {
+    return val1 + val2
+  }
+  return (val1 + val2) * 2
+}
+
+const getValueWithConditionTwo = (val1, val2) => {
+  if(val1 === 40 && val2 === 40) {
+    return val1 + val2
+  }
+  return ((val1 + val2) * 2)
+}
+
+const getValueWithConditionThree = function(val1, val2) {
+  if(val1 === 40 && val2 === 40) {
+    return val1 + val2
+  }
+  return (val1 + val2) * 2
+}
+
+// === TEST YOURSELF ===
+// Once you're finished run the test with "npm run test-3"
+// If the test has all tests passed, switch to the next exercise file
+// If any of the tests fails, refactor the code and run the test command after you've fixed the function
+
+
+// Do not delete or change the lines beneath
+const f = {
+  doesArrayIncludeItemsBetweenVals: doesArrayIncludeItemsBetweenVals || undefined,
+  getValueWithConditionOne: getValueWithConditionOne || undefined,
+  getValueWithConditionTwo: getValueWithConditionTwo || undefined,
+  getValueWithConditionThree: getValueWithConditionThree || undefined,
+}
+export { f };
